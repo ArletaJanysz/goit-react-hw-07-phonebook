b32b56713d0df8e9a61eeb9b4f9db9ee7ed915bf
ArletaJanysz <arleta.janysz@gmail.com>
2023-11-24 09:27:53 +0100

    contactReducer update

--- src/redux/contactReducer.js
+++ src/redux/contactReducer.js
@@ -1,26 +1,40 @@
 import { createSlice } from '@reduxjs/toolkit';
+import {
+  fetchContactsAsync,
+  addContactAsync,
+  deleteContactAsync,
+} from './actions';
 
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: {
     contacts: [],
-    filter: '',
+    status: 'idle',
+    error: null,
   },
-  reducers: {
-    addContact: (state, action) => {
-      state.contacts.push(action.payload);
-    },
-    removeContact: (state, action) => {
-      state.contacts = state.contacts.filter(
-        contact => contact.id !== action.payload
-      );
-    },
-    setFilter: (state, action) => {
-      state.filter = action.payload;
-    },
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(fetchContactsAsync.pending, state => {
+        state.status = 'loading';
+      })
+      .addCase(fetchContactsAsync.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.contacts = action.payload;
+      })
+      .addCase(fetchContactsAsync.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      })
+      .addCase(addContactAsync.fulfilled, (state, action) => {
+        state.contacts.push(action.payload);
+      })
+      .addCase(deleteContactAsync.fulfilled, (state, action) => {
+        state.contacts = state.contacts.filter(
+          contact => contact.id !== action.payload
+        );
+      });
   },
 });
-
-export const { addContact, removeContact, setFilter } = contactsSlice.actions;
 
 export default contactsSlice.reducer;
