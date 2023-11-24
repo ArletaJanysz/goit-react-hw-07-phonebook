d7fbd2178d646c4f03f3ecd33bf4635b5fd83009
ArletaJanysz <arleta.janysz@gmail.com>
2023-11-24 09:26:45 +0100

    createAsyncThunk added to actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,27 @@
-import { createAction } from '@reduxjs/toolkit';
 
-export const addContact = createAction('ADD_CONTACT');
-export const removeContact = createAction('REMOVE_CONTACT');
-export const setFilter = createAction('SET_FILTER');
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import * as api from '../services/api';
+
+export const fetchContactsAsync = createAsyncThunk(
+  'contacts/fetchContacts',
+  async () => {
+    const contacts = await api.fetchContacts();
+    return contacts;
+  }
+);
+
+export const addContactAsync = createAsyncThunk(
+  'contacts/addContact',
+  async contact => {
+    const newContact = await api.addContact(contact);
+    return newContact;
+  }
+);
+
+export const deleteContactAsync = createAsyncThunk(
+  'contacts/deleteContact',
+  async contactId => {
+    await api.deleteContact(contactId);
+    return contactId;
+  }
+);
